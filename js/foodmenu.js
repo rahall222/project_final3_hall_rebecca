dee6cb5a7a8d821cfcbb1a9070bdaa21bf45b465
Rebecca Hall <rahall222@gmail.com>
2015-04-12 19:07:45 +0100

    add stickup plugin to foodmenu submenu

--- js/foodmenu.js
+++ js/foodmenu.js
@@ -1,10 +1,24 @@
 $(document).ready( function() {
+
+// HAMBURGER MENU
 	$(".drawer").drawer();
 
+// FULL PAGE SCROLL
 	$('#fullpage').fullpage({
 		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
 		menu: '#subMenu',
 		sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
 		autoScrolling: false
 	});
+
+//STICK UP
+//initializing jQuery
+	jQuery(function($) {
+		//enabling stickUp on the '.navbar-wrapper' class
+		$('.navbar-wrapper').stickUp({
+			//enabling marginTop with the 'auto' setting
+			marginTop: 'auto'
+		});
+	});
+
 });
--- readme.txt
+++ readme.txt
@@ -4,4 +4,5 @@ Resources:
 http://git.blivesta.com/drawer/responsive-right/
 http://alvarotrigo.com/fullPage/
 http://stackoverflow.com/questions/5772192/git-how-can-i-reconcile-detached-head-with-master-origin
+http://lirancohen.github.io/stickUp/
 -
