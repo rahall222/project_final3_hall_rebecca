4c23b6a2a4b5ec888adb5aef0f8fcca508831413
Rebecca Hall <rahall222@gmail.com>
2015-04-12 17:53:46 +0100

    add foodmenu page, add subnav with jquery plugin fullpage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready( function() {
 	$(".drawer").drawer();
+
+	$('#fullpage').fullpage({
+		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+		menu: '#subMenu',
+		sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+		autoScrolling: false
+	});
 });
--- readme.txt
+++ readme.txt
@@ -2,4 +2,5 @@ URL:
 
 Resources:
 http://git.blivesta.com/drawer/responsive-right/
+http://alvarotrigo.com/fullPage/
 -
