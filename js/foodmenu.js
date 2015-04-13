d285b82b1c31e99be5601fe1d62cd3892a54aa04
Rebecca Hall <rebecca@doublestruck.co.uk>
2015-04-13 17:19:46 +0100

    add images to foodmenu, add drinks section, add favicon

--- js/foodmenu.js
+++ js/foodmenu.js
@@ -5,9 +5,9 @@ $(document).ready( function() {
 
 // FULL PAGE SCROLL
 	$('#fullpage').fullpage({
-		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
 		menu: '#subMenu',
-		sectionsColor: ['whitesmoke', 'whitesmoke', 'whitesmoke', 'whitesmoke'],
+		sectionsColor: ['whitesmoke', 'whitesmoke', 'whitesmoke', 'whitesmoke', 'whitesmoke'],
 		autoScrolling: false,
 		fitToSection: true
 	});
@@ -20,6 +20,47 @@ $(document).ready( function() {
 			//enabling marginTop with the 'auto' setting
 			marginTop: 'auto'
 		});
+	});
+
+//BACKSTRETCH
+	$(".breakfast1").backstretch("img/breakfast1.jpg", {
+		speed: 500
+	});
+
+	$(".breakfast2").backstretch("img/breakfast2.jpg", {
+		speed: 500
+	});
+
+	$(".breakfast3").backstretch("img/breakfast3.jpg", {
+		speed: 500
+	});
+
+	$(".sandwich1").backstretch("img/sandwich1.jpg", {
+		speed: 500
+	});
+
+	$(".sandwich3").backstretch("img/sandwich3.jpg", {
+		speed: 500
+	});
+
+	$(".hotdog1").backstretch("img/hotdog1.jpg", {
+		speed: 500
+	});
+
+	$(".dessert1").backstretch("img/dessert1.jpg", {
+		speed: 500
+	});
+
+	$(".dessert2").backstretch("img/dessert2.jpg", {
+		speed: 500
+	});
+
+	$(".dessert3").backstretch("img/dessert3.jpg", {
+		speed: 500
+	});
+
+	$(".drinks1").backstretch("img/drinks1.jpg", {
+		speed: 500
 	});
 
 });
