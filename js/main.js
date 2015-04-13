aaaaebb47753c13dda604c5c76fadfd9e772a6c8
Rebecca Hall <rebecca@doublestruck.co.uk>
2015-04-13 21:28:23 +0100

    add backstretch to index images and fix styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready( function() {
 	$(".drawer").drawer();
+
+//BACKSTRETCH
+	$(".chang").backstretch("img/chang.jpg", {
+		speed: 500
+	});
+
+	$(".community").backstretch("img/community.jpg", {
+		speed: 500
+	});
+
+
 });
